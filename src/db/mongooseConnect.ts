9897bee726686b0e1bba3f9b12fee2e50d86176d
vulebaolong <vulebaolong@gmail.com>
2023-08-03 00:52:22 +0700

    add note

--- src/db/mongooseConnect.ts
+++ src/db/mongooseConnect.ts
@@ -17,19 +17,25 @@ export const mongooseConnect = (): void => {
         });
 };
 
+// Kết nối MongoDB và in thông báo khi thành công:
 mongoose.connection.on("connected", () => {
     console.log("Mongodb connected to db");
 });
 
+// Xử lý lỗi khi kết nối MongoDB thất bại:
 mongoose.connection.on("error", (err) => {
     console.log(err.message);
 });
 
+// Ngắt kết nối với MongoDB và xử lý khi ứng dụng tắt:
 mongoose.connection.on("disconnected", () => {
     console.log('Mongodb connected is disconnected');
 });
 
+// Xử lý kết thúc ứng dụng Node.js: 
 process.on('SIGINT', async () => { 
     await mongoose.connection.close()
+    
+    // đảm bảo kết nối với MongoDB được đóng một cách đúng đắn
     process.exit(0)
  })
